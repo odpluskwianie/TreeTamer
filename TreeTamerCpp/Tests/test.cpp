4db88ea1a9d3114732f5ef89f7e2c710e996b445
jerzymh <01154178@pw.edu.pl>
2025-04-19 13:06:45 +0200

    iterate over xml example

--- TreeTamerCpp/Tests/test.cpp
+++ TreeTamerCpp/Tests/test.cpp
@@ -13,30 +13,42 @@ TEST(TestCaseName, TestName) {
   EXPECT_TRUE(true);
 }
 
+std::string indent(int level) {
+    std::string s;
+    for (int i = 0; i < level; i++) s += "  ";
+    return s;
+}
+
+void printTree(boost::property_tree::ptree& pt, int level) {///TO REMOVE
+    if (pt.empty()) {
+        std::cerr << "\"" << pt.data() << "\"";
+    }
+
+    else {
+        if (level) std::cerr << std::endl;
+
+        std::cerr << indent(level) << "{" << std::endl;
+
+        for (boost::property_tree::ptree::iterator pos = pt.begin(); pos != pt.end();) {
+            std::cerr << indent(level + 1) << "\"" << pos->first << "\": ";
+
+            printTree(pos->second, level + 1);
+            ++pos;
+            if (pos != pt.end()) {
+                std::cerr << ",";
+            }
+            std::cerr << std::endl;
+        }
+    }
+}
+
 TEST(XmlParserWork, OpenXml) {
 	namespace pt = boost::property_tree;
-    // Create empty property tree object
     pt::ptree tree;
-
-    // Parse the XML into the property tree.
     pt::read_xml("some.xml", tree);
-
-    // Use the throwing version of get to find the debug filename.
-    // If the path cannot be resolved, an exception is thrown.
-    std::string m_file = tree.get<std::string>("debug.filename");
-
-    // Use the default-value version of get to find the debug level.
-    // Note that the default value is used to deduce the target type.
-    //std:: = tree.get("debug.level", 0);
-
-    // Use get_child to find the node containing the modules, and iterate over
-    // its children. If the path cannot be resolved, get_child throws.
-    // A C++11 for-range loop would also work.
-    //BOOST_FOREACH(pt::ptree::value_type & v, tree.get_child("debug.modules")) {
-    //    // The data function is used to access the data stored in a node.
-    //    m_modules.insert(v.second.data());
-    //}
-
+    std::string m_file = tree.get<std::string>("debug.filename");   
+	printTree(tree, 0);
+;
 	EXPECT_NE(1, 0);
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
